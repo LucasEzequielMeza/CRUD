8da80129edf9eb5201030532412223988af43bf9
Lucas Ezequiel Meza <lucasezequiel9720@hotmail.com>
2025-01-16 15:42:15 -0300

    Validacíón de token

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+
 import router from './routes/auth.routes.js';
+import taskRouter from './routes/tasks.routes.js';
 
 const app = express();
 
@@ -8,6 +11,9 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.use("/api/",router);
+app.use("/api/",taskRouter);
 
 export default app;
